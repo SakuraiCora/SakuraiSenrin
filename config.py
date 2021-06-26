0663c6936b5f768c8251c0e559059b5243a4c469
SakuraiCora <52453199+Hajimarino-HOPE@users.noreply.github.com>
2021-06-26 16:22:25 +0800

    [Modify] 新增搜图API

--- config.py
+++ config.py
@@ -3,11 +3,13 @@
 
     baidu_cilent_id & baidu_cilent_secret请从 https://cloud.baidu.com 获取
     lolicon_api 请从 https://api.lolicon.app 获取
+    SauceNAO_api 请从 https://saucenao.com 获取
 """
 baidu_client_id = ''
 baidu_client_secret = ''
 
-lolicon_api = ''
+lolicon_api = ''    
+SauceNAO_api = ''
 
 masterList = ['']  # bot管理员 (str)
 AdminList_REPORT = []   #息上报的QQ号码 (int)
